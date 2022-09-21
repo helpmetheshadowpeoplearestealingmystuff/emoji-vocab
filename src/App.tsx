2d2fba5873369a312088fccee5de8ce9ec76afc5
pp.pn <git@pp.pn>
2022-09-21 05:54:15 +0200

    feat: dark/light theme implemented, switched to onchange

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { createTheme, Input, Spacer, useTheme } from '@nextui-org/react';
+import { createTheme, Input, Spacer, useTheme, NextUIProvider } from '@nextui-org/react';
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import React, { useState } from 'react';
 import './App.css';
 import Twemoji from './Twemoji';
@@ -6,12 +7,17 @@ import keywords from 'emoji-synonyms';
 type Synonyms = { [key: string]: string[] };
 const synonyms = keywords as Synonyms;
 
+const lightTheme = createTheme({
+  type: 'light',
+});
+
 const darkTheme = createTheme({
   type: 'dark',
-  // theme: {
-  //   colors: {...},
-  // }
-})
+  theme: {
+    colors: {
+      background: "282c34"
+  }}
+});
 
 const item: {emoji: string, keywords: string[], synonyms: string[]}[] = [
   {emoji: '👋', synonyms: ['wave'], keywords: synonyms['wave']},
@@ -19,44 +25,71 @@ const item: {emoji: string, keywords: string[], synonyms: string[]}[] = [
   {emoji: '❤️', synonyms: ['heart'], keywords: synonyms['heart']},
 ]
 
-
 function App() {
   const [index, setIndex] = useState(0);
   const [score, setScore] = useState(0);
   const [runningFailures, setRunningFailures] = useState(0);
-  const { theme } = useTheme();
-  if (!theme) return null;
+  const [answer, setAnswer] = useState('');
+  /* const handleChange = (e) => {
+   *   setAnswer(value);
+   * }; */
+  const submitAnswer = (value: string) => {
+    if (item[index].synonyms.find(synonym => synonym === answer.toLowerCase())) {
+      setIndex(index + 1);
+      setScore(score + 1);
+      setRunningFailures(0);
+    } else {
+      setIndex(index + 1);
+      setRunningFailures(runningFailures + 1);
+      if (runningFailures > 3) {
+        // TODO: Navigaet to score screen
+      }
+    }
+    if (index > item.length) {
+      setIndex(0);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <Twemoji emoji={item[index].emoji} />
-        <Spacer y={4} />
-        <Input
-          bordered
-          size="xl"
-          style={{
-            color: theme.colors.primary.value,
-            fontSize: theme.fontSizes.xl.value,
-            padding: `${theme.space[2].value} ${theme.space[4].value}`
-          }}
-          onBlur={function(e) {
-            if (item[index].synonyms.find(i => i ===e.target.value)) {
-              setIndex(index + 1);
-              setScore(score + 1);
-              setRunningFailures(0);
-              if (index > item.length) {
-                // end
-                setIndex(0);
+    <NextThemesProvider
+      defaultTheme="system"
+      attribute="class"
+      value={{
+        light: lightTheme.className,
+        dark: darkTheme.className
+      }}
+    >
+      <NextUIProvider>
+        <div className="App"
+             style={{
+               background: "$background",
+             }}
+        >
+          <header className="App-header">
+            <Twemoji emoji={item[index].emoji} />
+            <Spacer y={4} />
+            <Input
+              value={answer}
+              clearable
+              bordered
+              size="xl"
+              style={{
+                background: "$background",
+                color: "$primary",
+                fontSize: "$xl",
+              }}
+              onChange={(e) => {
+                setAnswer(e.target.value);}
               }
-            } else {
-
-            }
-          }}
-          labelPlaceholder="emoji name"
-          color="default" />
-        <Spacer y={4} />
-      </header>
-    </div>
+              onBlur={(e) => {
+                submitAnswer(e.target.value);}
+              }
+              labelPlaceholder="emoji name"
+              color="default" />
+            <Spacer y={4} />
+          </header>
+        </div>
+      </NextUIProvider>
+    </NextThemesProvider>
   );
 }
 
