aae62bd32a7777f3725ac21f2fd44f049633c44b
real <real@email>
2022-09-21 03:18:07 +0200

    MVP done, without scoring mechanism or ending

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+// basic happy path for 0 points.
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  /* render(<App />);
+   * const linkElement = screen.getByText(/learn react/i);
+   * expect(linkElement).toBeInTheDocument(); */
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import { createTheme, Input, Spacer, useTheme } from '@nextui-org/react';
+import React, { useState } from 'react';
 import './App.css';
+import Twemoji from './Twemoji';
+import keywords from 'emoji-synonyms';
+type Synonyms = { [key: string]: string[] };
+const synonyms = keywords as Synonyms;
+
+const darkTheme = createTheme({
+  type: 'dark',
+  // theme: {
+  //   colors: {...},
+  // }
+})
+
+const item: {emoji: string, keywords: string[], synonyms: string[]}[] = [
+  {emoji: '👋', synonyms: ['wave'], keywords: synonyms['wave']},
+  {emoji: '😂', synonyms: ['joy'], keywords: synonyms['joy']},
+  {emoji: '❤️', synonyms: ['heart'], keywords: synonyms['heart']},
+]
+
 
 function App() {
+  const [index, setIndex] = useState(0);
+  const [score, setScore] = useState(0);
+  const [runningFailures, setRunningFailures] = useState(0);
+  const { theme } = useTheme();
+  if (!theme) return null;
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Twemoji emoji={item[index].emoji} />
+        <Spacer y={4} />
+        <Input
+          bordered
+          size="xl"
+          style={{
+            color: theme.colors.primary.value,
+            fontSize: theme.fontSizes.xl.value,
+            padding: `${theme.space[2].value} ${theme.space[4].value}`
+          }}
+          onBlur={function(e) {
+            if (item[index].synonyms.find(i => i ===e.target.value)) {
+              setIndex(index + 1);
+              setScore(score + 1);
+              setRunningFailures(0);
+              if (index > item.length) {
+                // end
+                setIndex(0);
+              }
+            } else {
+
+            }
+          }}
+          labelPlaceholder="emoji name"
+          color="default" />
+        <Spacer y={4} />
       </header>
     </div>
   );
