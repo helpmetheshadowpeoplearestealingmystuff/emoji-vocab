fec775fbdb7f4846d61cfd7d5c9376e66437cc7a
pp.pn <git@pp.pn>
2022-09-22 01:37:35 +0200

    feat: seperate components, add score screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { createTheme, Input, Spacer, useTheme, NextUIProvider } from '@nextui-org/react';
+import { createTheme, NextUIProvider } from '@nextui-org/react';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
-import { useState, createContext } from 'react';
+import { useState } from 'react';
 import './App.css';
-import Twemoji from './Twemoji';
-import keywords from 'emoji-synonyms';
-type Synonyms = { [key: string]: string[] };
-const synonyms = keywords as Synonyms;
+import Test from './Test';
 
 const lightTheme = createTheme({
   type: 'light',
@@ -19,45 +16,33 @@ const darkTheme = createTheme({
   }}
 });
 
-// implement bloom filter
-const items: {emoji: string, keywords: string[], synonyms: string[]}[] = [
-  {emoji: '👋', synonyms: ['wave'], keywords: synonyms['wave']},
-  {emoji: '😂', synonyms: ['joy'], keywords: synonyms['joy']},
-  {emoji: '❤️', synonyms: ['heart'], keywords: synonyms['heart']},
-]
-
 function App() {
   const [index, setIndex] = useState(0);
-  const [score, setScore] = useState(0);
   const [answers, setAnswers] = useState<string[]>([]);
-  const [runningFailures, setRunningFailures] = useState(0);
-  const [answer, setAnswer] = useState('');
-  /* const handleChange = (e) => {
-   *   setAnswer(value);
-   * }; */
-  const submitAnswer = () => {
-    setAnswers(answers => [...answers, answer]);
-    setAnswer("");
-    if (items[index].synonyms.find(synonym => synonym === answer.toLowerCase())) {
-      setIndex(index + 1);
-      setScore(score + 1);
-      setRunningFailures(0);
-    } else {
-      setIndex(index + 1);
-      setRunningFailures(runningFailures + 1);
-      if (runningFailures >= 3) {
-        /* console.log(btoa(answers.join())); */
-        // TODO: Navigate to score screen
-      }
-    }
-    if (index >= items.length - 1) {
-      /* console.log(answers);
-       * console.log(btoa(answers.join(":")));
-       * console.log(atob(btoa(answers.join(":")))); */
-      // TODO: Navigate to score screen
-      setIndex(0);
-    }
-  };
+  const [ended, setEnded] = useState(false);
+  if (ended) {
+    return (
+      <NextThemesProvider
+        defaultTheme="system"
+        attribute="class"
+        value={{
+          light: lightTheme.className,
+          dark: darkTheme.className
+        }}
+      >
+        <NextUIProvider>
+    <div className="App"
+         style={{
+           background: "$background",
+         }}
+    >
+        <div className="App-content">
+          score: {answers.join(" ")}
+        </div>
+      </div>
+        </NextUIProvider>
+      </NextThemesProvider>
+)} else {
   return (
     <NextThemesProvider
       defaultTheme="system"
@@ -73,41 +58,18 @@ function App() {
                background: "$background",
              }}
         >
-          <div className="App-content">
-            <Twemoji emoji={items[index].emoji} />
-            <Spacer y={4} />
-            <Input
-              value={answer}
-              clearable
-              bordered
-              pattern="\w+"
-              size="xl"
-              style={{
-                background: "$background",
-                color: "$primary",
-                fontSize: "$xl",
-              }}
-              onChange={(e) => {
-                if (!e.target.value.match(/\W/)) {
-                  setAnswer(e.target.value);
-                }
-              }}
-              onKeyDown={(e) => {
-                if (e.code === "Enter" || e.code === "NumpadEnter") {
-                  submitAnswer();
-                }
-                if (!e.key.match(/[a-zA-Z]/)) {
-                  e.preventDefault();
-                }
-              }}
-              labelPlaceholder="emoji name"
-              color="default" />
-            <Spacer y={4} />
-          </div>
+          <Test
+            index={index}
+            setIndex={setIndex}
+            answers={answers}
+            setAnswers={setAnswers}
+            isEnded={setEnded}
+          />
         </div>
       </NextUIProvider>
     </NextThemesProvider>
-  );
+  )
+}
 }
 
-export default App;
+      export default App;
---
+++ src/Test.tsx
@@ -0,0 +1,86 @@
+import { createTheme, Input, Spacer, useTheme, NextUIProvider } from '@nextui-org/react';
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
+import { useState, createContext } from 'react';
+import './App.css';
+import Twemoji from './Twemoji';
+
+// The top ten emoji used worldwide are 😂 ❤️ 🤣 👍 😭 🙏 😘 🥰 😍 😊
+// https://home.unicode.org/emoji/emoji-frequency/
+// TODO: implement bloom filter
+const items: {emoji: string, synonyms: string[]}[] = [
+  {emoji: '👋', synonyms: ['wave']},
+  {emoji: '😂', synonyms: ['joy']},
+  {emoji: '❤️', synonyms: ['heart']},
+  {emoji: '🙏️', synonyms: ['pray']},
+  {emoji: '😘️', synonyms: ['kiss']},
+  {emoji: '🥰️', synonyms: ['hearts']},
+  {emoji: '😍️', synonyms: ['hearteyes']},
+  {emoji: '😊️', synonyms: ['smile']},
+  {emoji: '👍️', synonyms: ['thumbsup']},
+]
+
+function Test({index, setIndex, answers, setAnswers, isEnded}: {index: number, setIndex: React.Dispatch<React.SetStateAction<number>>, answers: string[], setAnswers: React.Dispatch<React.SetStateAction<string[]>>, isEnded: React.Dispatch<React.SetStateAction<boolean>>}) {
+  const [runningFailures, setRunningFailures] = useState(0);
+  const [answer, setAnswer] = useState('');
+  /* const handleChange = (e) => {
+   *   setAnswer(value);
+   * }; */
+  const submitAnswer = () => {
+    setAnswers(answers => [...answers, answer]);
+    setAnswer('');
+    if (items[index].synonyms.find(synonym => synonym === answer.toLowerCase())) {
+      setIndex(index + 1);
+      setRunningFailures(0);
+    } else {
+      setIndex(index + 1);
+      setRunningFailures(runningFailures + 1);
+      if (runningFailures >= 3) {
+        isEnded(true);
+      }
+    }
+    if (index >= items.length - 1) {
+      isEnded(true);
+    }
+  };
+  return (
+    <div className="App"
+         style={{
+           background: "$background",
+         }}
+    >
+      <div className="App-content">
+        <Twemoji emoji={items[index].emoji} />
+        <Spacer y={4} />
+        <Input
+          value={answer}
+          clearable
+          bordered
+          pattern="\w+"
+          size="xl"
+          style={{
+            background: "$background",
+            color: "$primary",
+            fontSize: "$xl",
+          }}
+          onChange={(e) => {
+            if (!e.target.value.match(/\W/)) {
+              setAnswer(e.target.value);
+            }
+          }}
+          onKeyDown={(e) => {
+            if (e.code === "Enter" || e.code === "NumpadEnter") {
+              submitAnswer();
+            }
+            if (!e.key.match(/[a-zA-Z]/)) {
+              e.preventDefault();
+            }
+          }}
+          labelPlaceholder="emoji name"
+          color="default" />
+        <Spacer y={4} />
+      </div>
+    </div>
+  );
+}
+
+export default Test;
